65ceacae2e355642941da5da1a18a9c982241e5e
Rany Keddo <rany@soundcloud.com>
2013-08-10 23:08:46 +0200

    Travis CI

---
+++ README.md
@@ -0,0 +1,6 @@
+# Towers of Hanoi
+
+[![Build Status](https://travis-ci.org/purzelrakete/hanoi.png)](https://travis-ci.org/purzelrakete/hanoi)
+
+A general way of doing `A*`, applied to towers of hanoi. As described by Peter
+Norvig.
---
+++ test.rb
@@ -0,0 +1,16 @@
+$: << File.expand_path(File.dirname(__FILE__))
+
+require 'render'
+require 'search'
+require 'problem'
+require 'node'
+require "test/unit"
+
+class TestHanoi < Test::Unit::TestCase
+  def test_result
+    problem = Problem.new(disks = 3 , pegs = 5)
+    search  = Search.new(problem)
+    goal = search.run
+    assert_equal([[], [], [], [], [1, 2, 3]], goal.state)
+  end
+end
